01da21310ca48f216592eda55abb511e9cc1445d
David Andersson <nderssonpublic@gmail.com>
2020-11-22 20:28:51 +1100

    add env

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { InfrastructureStack } from '../lib/infrastructure-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { InfrastructureStack } from "../lib/infrastructure-stack";
+
+const env = {
+  account: process.env.AWS_ACCOUNT,
+  region: process.env.AWS_DEFAULT_REGION,
+};
 
 const app = new cdk.App();
-new InfrastructureStack(app, 'InfrastructureStack');
+new InfrastructureStack(app, "InfrastructureStack", { env });
