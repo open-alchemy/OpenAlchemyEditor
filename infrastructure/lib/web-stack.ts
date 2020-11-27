21b34ab40a4720af551d9d4535632c7823b37d85
David Andersson <nderssonpublic@gmail.com>
2020-11-27 22:51:37 +1100

    add bucket name

--- infrastructure/lib/web-stack.ts
+++ infrastructure/lib/web-stack.ts
@@ -2,12 +2,16 @@ import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3Deployment from "@aws-cdk/aws-s3-deployment";
 
+import { CONFIG } from "./config";
+
 export class WebStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // S3 bucket
-    const bucket = new s3.Bucket(this, "Bucket");
+    const bucket = new s3.Bucket(this, "Bucket", {
+      bucketName: `${CONFIG.web.recordName}.${CONFIG.domainName}`,
+    });
     new s3Deployment.BucketDeployment(this, "BucketDeployment", {
       sources: [s3Deployment.Source.asset("resources/web")],
       destinationBucket: bucket,
