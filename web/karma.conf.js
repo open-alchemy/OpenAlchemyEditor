62d6afa0e39cbe585c415b9b1580ff3019f04ed2
David Andersson <nderssonpublic@gmail.com>
2020-11-28 13:51:32 +1100

    add global service test and enforce coverage

--- web/karma.conf.js
+++ web/karma.conf.js
@@ -11,18 +11,28 @@ module.exports = function (config) {
       require("karma-jasmine"),
       require("karma-chrome-launcher"),
       require("karma-jasmine-html-reporter"),
-      require("karma-coverage-istanbul-reporter"),
+      require("karma-coverage"),
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
-      dir: require("path").join(__dirname, "./coverage/editor"),
-      reports: ["html", "lcovonly", "text-summary"],
-      fixWebpackSourcePaths: true,
+    preprocessors: {
+      "src/**/*.ts": ["coverage"],
     },
-    reporters: ["progress", "kjhtml"],
+    reporters: ["progress", "kjhtml", "coverage"],
+    coverageReporter: {
+      type: "html",
+      dir: "coverage/",
+      check: {
+        global: {
+          statements: 100,
+          branches: 100,
+          functions: 100,
+          lines: 100,
+        },
+      },
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- web/src/app/components/base/base.component.ts
+++ web/src/app/components/base/base.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-base',
   templateUrl: './base.component.html',
   styleUrls: ['./base.component.css'],
 })
-export class BaseComponent implements OnInit {
+export class BaseComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
